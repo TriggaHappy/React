b195fda6b624083fbaf84fb8fa5d035f0475f56e
Maximilian Merten <maximilian.merten@ewerk.com>
2025-01-03 13:06:20 +0100

    feature: adds entry and jokes component + navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import MainContent from './components/section 1/MainContent.jsx';
-import Navbar from './components/section 1/Navbar.jsx';
-import './section 1/section_one_index.css';
+import './index_section_one/section_one_index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import Footer from './components/section 1/Footer.jsx';
-import backgroundImage from './assets/section 1/react-background.png';
+import Navbar_two from './components/section_two/Navbar_two.jsx';
+import Entry from './components/section_two/Entry.jsx';
+import Joke from './components/section_two/Joke.jsx';
 
 export default function App() {
-  console.log(backgroundImage);
+  const jokes = {
+    setup_one: 'I got my daughter a fridge for her birthday.',
+    punchline_one: "I can't wait to see her face light up when she opens it",
+    setup_two: 'How did the hacker escape the police.',
+    punchline_two: 'He just ransomware!',
+    setup_three: "Why don't pirates travel on mountain roads?",
+    punchline_three: 'Scurvy',
+    setup_four: 'Why do bees stay in the hive in the winter?',
+    punchline_four: 'Swarm.',
+  };
 
   return (
     <>
-      <Navbar />
-      <MainContent />
-      <Footer />
+      <Navbar_two />
+      <Entry />
+      <Joke setup={jokes.setup_one} punchline={jokes.punchline_one} />
+      <Joke setup={jokes.setup_two} punchline={jokes.punchline_two} />
+      <Joke setup={jokes.setup_three} punchline={jokes.punchline_three} />
+      <Joke setup={jokes.setup_four} punchline={jokes.punchline_four} />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './section 1/section_one_index.css';
+import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
