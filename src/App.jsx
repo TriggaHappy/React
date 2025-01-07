65997da4f38ae697e2efe2ab911e62f75e5ebc6c
Maximilian Merten <maximilian.merten@ewerk.com>
2025-01-07 13:44:01 +0100

    chore: refactors mapping and conciseness

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
-import './index_section_one/section_one_index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Navbar_two from './components/section_two/Navbar_two.jsx';
 import Entry from './components/section_two/Entry.jsx';
 import travelData from './assets/section_two/section_two_data/travelData.js';
-import jokesData from './assets/section_two/section_two_data/jokesData.js';
-import Joke from './components/section_two/Joke.jsx';
 
 export default function App() {
-  const jokeElements = jokesData.map((joke) => {
-    return <Joke punchline={joke.punchline} setup={joke.setup} />;
+  const entryElements = travelData.map((entry) => {
+    return <Entry key={entry.id} entry={entry} />;
   });
 
   return (
     <>
       <Navbar_two />
-      {travelData.map((data, index) => (
-        <Entry
-          key={index}
-          title={data.title}
-          location={data.location}
-          googleMapsUrl={data.googleMapsUrl}
-          startDate={data.startDate}
-          endDate={data.endDate}
-          description={data.description}
-          img={{ src: data.imageUrl, alt: 'Mount Fuji' }}
-        />
-      ))}
-      {jokeElements}
+      <main>{entryElements}</main>
     </>
   );
 }
