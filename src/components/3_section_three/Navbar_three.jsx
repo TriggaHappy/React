8b42ec78eb2a70dcfe55c3cc1e87dca229ffc497
Maximilian Merten <maximilian.merten@ewerk.com>
2025-01-08 14:10:52 +0100

    chore: adds styling to the navbar

--- src/components/3_section_three/Navbar_three.jsx
+++ src/components/3_section_three/Navbar_three.jsx
@@ -3,7 +3,7 @@ import Chef from '../../assets/3_section_three/chef_claude.svg';
 export default function Navbar_three() {
   return (
     <>
-      <main className="flex justify-center items-center h-1/6 pt-3 pb-3 shadow-lg">
+      <main className="flex justify-center items-center h-1/6 pt-3 pb-3 shadow-lg mb-2">
         <img src={Chef} className="h-15 w-15" />
         <span className="pl-2 text-3xl">Chef Claude</span>
       </main>
