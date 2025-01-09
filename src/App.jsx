5f7431bbdfedc8b493a8131a4d60637e5a841c18
Maximilian Merten <maximilian.merten@ewerk.com>
2025-01-09 17:10:31 +0100

    chore: adds StatePractice component, uncomments not used components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Entry from './components/2_section_two/Entry.jsx';
 import travelData from './assets/2_section_two/section_two_data/travelData.js';
 import Navbar_three from './components/3_section_three/Navbar_three.jsx';
 import Main from './components/3_section_three/Main.jsx';
+import StatePractice from './components/3_section_three/StatePractice.jsx';
 
 export default function App() {
   /*section Two
@@ -22,8 +23,9 @@ export default function App() {
 
   return (
     <>
-      <Navbar_three />
-      <Main />
+      {/*<Navbar_three />*/}
+      {/*<Main />*/}
+      <StatePractice />
     </>
   );
 }
