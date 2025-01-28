1b42d5e1f2e33d54dbeba2d109074a705aaad97a
Maximilian Merten <19maximilianmerten96@gmail.com>
2025-01-28 14:12:18 +0100

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="mt-auto bg-gray-800">
+        <section>© 2024 Max Merten development. All rights reserved.</section>
+      </footer>
+    </>
+  );
+}
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,18 @@
+export default function MainContent() {
+  return (
+    <>
+      <main className="p-10 flex flex-col min-h-screen bg-gray-500 items-center">
+        <section className="mt-4 ml-3 text-3xl">
+          <h1 className="mb-2 font-extrabold">Fun Facts about React</h1>
+          <ul className="list-disc pl-5 custom-list space-y-2">
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k Stars on GitHub</li>
+            <li>Is maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+        </section>
+      </main>
+    </>
+  );
+}
