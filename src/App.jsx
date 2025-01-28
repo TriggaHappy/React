1f9836421a4c4046eb86d1ac84a7c3e16cd25fc9
Maximilian Merten <19maximilianmerten96@gmail.com>
2025-01-28 14:12:18 +0100

    chore: rearanges first section files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import MainContent from './components/MainContent.jsx';
-import Navbar from './components/Navbar.jsx';
-import './index.css';
+import MainContent from './components/section 1/MainContent.jsx';
+import Navbar from './components/section 1/Navbar.jsx';
+import './section 1/section_one_index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import Footer from './components/Footer.jsx';
-import backgroundImage from './assets/react-background.png';
+import Footer from './components/section 1/Footer.jsx';
+import backgroundImage from './assets/section 1/react-background.png';
 
 export default function App() {
   console.log(backgroundImage);
