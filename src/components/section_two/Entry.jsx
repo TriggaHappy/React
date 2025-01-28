8b7abdf02efe693d0a9ccb3841ba7334a6d39043
Maximilian Merten <19maximilianmerten96@gmail.com>
2025-01-28 14:13:38 +0100

    chore: refactors object passing

--- src/components/section_two/Entry.jsx
+++ src/components/section_two/Entry.jsx
@@ -2,29 +2,32 @@ import Fuji from '../../assets/section_two/mt_fuji.jpg';
 import Marker from '../../assets/section_two/marker.svg';
 
 export default function Entry(props) {
+  const entryData = props.entry;
+
   return (
     <>
       <section className="flex text-black h-60 pt-10 pl-10 justify-center items-center">
         <img
           className="rounded h-4/5 w-40"
-          src={props.img.src}
-          alt={props.img.alt}
+          src={entryData.img.src}
+          alt={entryData.img.alt}
         />
-        <div className="pl-3 pt-2 w-1/4">
+        <div className="pl-3 w-1/4">
           <header className="flex">
             <img src={Marker} />
             <div className="pl-2">
-              {props.location}
-              <a className="pl-2 text-blue-600" href={props.googleMapsUrl}>
+              {entryData.location}
+              <span>{entryData.country}</span>
+              <a className="pl-2 text-blue-600" href={entryData.googleMapsUrl}>
                 View on Google Maps
               </a>
             </div>
           </header>
-          <h1 className="font-extrabold text-2xl pt-2 pb-3">{props.title}</h1>
-          <h3 className="font-bold">
-            {props.startDate} - {props.endDate}
-          </h3>
-          <div className="text-xs">{props.description}</div>
+          <h1 className="font-extrabold text-2xl pt-2 pb-3">
+            {entryData.title}
+          </h1>
+          <h3 className="font-bold">{entryData.dates}</h3>
+          <div className="text-xs">{entryData.text}</div>
         </div>
       </section>
     </>
