c0fc799b23f80aeda93559fd9b430b9a956eda10
Maximilian Merten <19maximilianmerten96@gmail.com>
2025-01-28 14:11:33 +0100

    feature: adds form actions for learning forms

---
+++ src/components/3_section_three/Forms.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function Forms() {
+  function signUp(formData) {
+    const email = formData.get('email');
+    const password = formData.get('password');
+    console.log(email);
+    console.log(password);
+  }
+
+  return (
+    <section>
+      <h1>Signup form</h1>
+      <form action={signUp}>
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="joe@schmoe.com"
+        />
+        <br />
+
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password" />
+        <br />
+
+        <button>Submit</button>
+      </form>
+    </section>
+  );
+}
